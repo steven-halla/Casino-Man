2e0a2146a359aa036767bee15cd9ca79851a7f46
steven-halla <steven.r.halla@gmail.com>
2023-12-17 19:24:40 -0800

    stripping out more logic out of main.py and putting it in seperate file

---
+++ Game/game_control.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class GameControl:
+    def __init__(self, red_rect, blue_rect, screen_width, screen_height, speed):
+        self.red_rect = red_rect
+        self.blue_rect = blue_rect
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.speed = speed
+
+    def handle_keys(self, keys):
+        dx, dy = 0, 0
+        if keys[pygame.K_LEFT]:
+            dx = -self.speed
+        if keys[pygame.K_RIGHT]:
+            dx = self.speed
+        if keys[pygame.K_UP]:
+            dy = -self.speed
+        if keys[pygame.K_DOWN]:
+            dy = self.speed
+
+        self.red_rect.move(dx, dy, [self.blue_rect], self.screen_width, self.screen_height)
