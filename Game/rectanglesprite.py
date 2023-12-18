c68d9c986bc8d00a81f7a8c88529681ff70582da
steven-halla <steven.r.halla@gmail.com>
2023-12-17 19:20:37 -0800

    added file structure

---
+++ Game/rectanglesprite.py
@@ -0,0 +1,38 @@
+import pygame
+
+class RectangleSprite(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
+    def move(self, dx, dy, barriers, screen_width, screen_height):
+        # Move each axis separately and check for collisions
+        self.rect.x += dx
+        self.collide(dx, 0, barriers)
+        self.rect.y += dy
+        self.collide(0, dy, barriers)
+
+        # Check for screen boundaries
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def collide(self, dx, dy, barriers):
+        # Check for collision with barriers
+        for barrier in barriers:
+            if self.rect.colliderect(barrier.rect):
+                if dx > 0:  # Moving right; hit the left side of the barrier
+                    self.rect.right = barrier.rect.left
+                if dx < 0:  # Moving left; hit the right side of the barrier
+                    self.rect.left = barrier.rect.right
+                if dy > 0:  # Moving down; hit the top side of the barrier
+                    self.rect.bottom = barrier.rect.top
+                if dy < 0:  # Moving up; hit the bottom side of the barrier
+                    self.rect.top = barrier.rect.bottom
--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 import pygame
+from Game.rectanglesprite import RectangleSprite
+
+# ... rest of your main.py code ...
+
+
+# ... rest of your main.py code ...
+
+
 
 # Define a Rectangle class that inherits from pygame.sprite.Sprite
-class Rectangle(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-
-    def move(self, dx, dy, barriers, screen_width, screen_height):
-        # Move each axis separately and check for collisions
-        self.rect.x += dx
-        self.collide(dx, 0, barriers)
-        self.rect.y += dy
-        self.collide(0, dy, barriers)
-
-        # Check for screen boundaries
-        if self.rect.right > screen_width:
-            self.rect.right = screen_width
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.bottom > screen_height:
-            self.rect.bottom = screen_height
-        if self.rect.top < 0:
-            self.rect.top = 0
-
-    def collide(self, dx, dy, barriers):
-        # Check for collision with barriers
-        for barrier in barriers:
-            if self.rect.colliderect(barrier.rect):
-                if dx > 0:  # Moving right; hit the left side of the barrier
-                    self.rect.right = barrier.rect.left
-                if dx < 0:  # Moving left; hit the right side of the barrier
-                    self.rect.left = barrier.rect.right
-                if dy > 0:  # Moving down; hit the top side of the barrier
-                    self.rect.bottom = barrier.rect.top
-                if dy < 0:  # Moving up; hit the bottom side of the barrier
-                    self.rect.top = barrier.rect.bottom
-
 
 # Initialize Pygame
 pygame.init()
@@ -48,8 +19,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Moving Red Rectangle")
 
 # Create instances of the Rectangle class
-red_rect = Rectangle((255, 0, 0), 100, 50)
-blue_rect = Rectangle((0, 0, 255), 100, 50)
+red_rect = RectangleSprite((255, 0, 0), 100, 50)
+blue_rect = RectangleSprite((0, 0, 255), 100, 50)
 
 # Set initial positions
 red_rect.rect.x = (screen_width - red_rect.rect.width) // 2
